bea81515a4271c9acfe85a746e225404a151f885
GaryRoberts <garyroberts1995@gmail.com>
2020-08-02 21:21:02 -0500

    updated filtering functionality

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
 function filter_table(options)
 {
-  document.getElementById(options.input_field).addEventListener("keyup", myFunction);
-  
-    var input, filter, table, tr, td, i, txtValue;
+  document.getElementById(options.input_field).addEventListener("keyup",buffering(options));
+    
+}
 
-    if(options.table_id!=null)
-    {
-      input = document.getElementById(options.input_field);
-      table = document.getElementById(options.table_id);
+function buffering(options)
+{
+  var input, filter, table, tr, td, i, txtValue;
 
-    filter = input.value.toUpperCase();
-   
-    tr = table.getElementsByTagName("tr");
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td")[0];
-      if (td) {
-        txtValue = td.textContent || td.innerText;
-        if (txtValue.toUpperCase().indexOf(filter) > -1) {
-          tr[i].style.display = "";
-        } else {
-          tr[i].style.display = "none";
-        }
-      }       
-    }
+  if(options.table_id!=null)
+  {
+    input = document.getElementById(options.input_field);
+    table = document.getElementById(options.table_id);
+
+  filter = input.value.toUpperCase();
+ 
+  tr = table.getElementsByTagName("tr");
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }       
   }
+}
 }
 
 module.exports.filter_table=filter_table;
