e80636c97799654129a4bf743cc40cd0219010fb
GaryRoberts <garyroberts1995@gmail.com>
2020-08-02 19:30:47 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+function filter_table(options)
+{
+    var input, filter, table, tr, td, i, txtValue;
+
+    if(options.input_field)
+    input = document.getElementById(options.input_field);
+    
+    if(options.table_id)
+    table = document.getElementById(options.table_id);
+
+    filter = input.value.toUpperCase();
+   
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[0];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
+
+}
+
+module.exports.filter_table=filter_table;
