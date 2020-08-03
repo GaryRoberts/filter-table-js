7db608477b15e52feac74b0ddf493ea8404e527c
Gary Roberts <35618554+GaryRoberts@users.noreply.github.com>
2020-08-02 23:37:20 -0500

    Added column option to filter function

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 function filter_table(options)
 {
-  document.getElementById(options.input_field).addEventListener("keyup",buffering(options));
     
-}
+    var input, filter, table, tr, td, i, txtValue;
 
-function buffering(options)
-{
-  var input, filter, table, tr, td, i, txtValue;
+    if(options.input_field!=null && options.table_id!=null && options.column!=null)
+    {
+      input = document.getElementById(options.input_field);
+      table = document.getElementById(options.table_id);
 
-  if(options.table_id!=null)
-  {
-    input = document.getElementById(options.input_field);
-    table = document.getElementById(options.table_id);
-
-  filter = input.value.toUpperCase();
- 
-  tr = table.getElementsByTagName("tr");
-  for (i = 0; i < tr.length; i++) {
-    td = tr[i].getElementsByTagName("td")[0];
-    if (td) {
-      txtValue = td.textContent || td.innerText;
-      if (txtValue.toUpperCase().indexOf(filter) > -1) {
-        tr[i].style.display = "";
-      } else {
-        tr[i].style.display = "none";
-      }
-    }       
+    filter = input.value.toUpperCase();
+   
+    tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) {
+      td = tr[i].getElementsByTagName("td")[options.column];
+      if (td) {
+        txtValue = td.textContent || td.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+        } else {
+          tr[i].style.display = "none";
+        }
+      }       
+    }
   }
 }
-}
+
 
 module.exports.filter_table=filter_table;
